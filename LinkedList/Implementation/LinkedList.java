a572fe9e50956c0b62500366a7d929021ecd6a0d
K VEERESH <kveeresh288@gmail.com>
2025-05-09 16:55:08 +0530

    addedPrintInsertionFrontLast

--- LinkedList/Implementation/LinkedList.java
+++ LinkedList/Implementation/LinkedList.java
@@ -13,29 +13,53 @@ public class LinkedList{
     static Node Tail;
 
     void addFirst(int data){
+         //step1 create a new Node 
+         Node newNode=new Node(data);
         if(Head==null){
-            Head=new Node(data);
-            Tail=Head;
+            Head=Tail=newNode;
             return;
         }
-        //step1 create a new Node 
-        Node newNode=new Node(data);
         //step 2 link newNode->next to the address of Head
         newNode.next=Head;
         //step 3 point newNode as a Head ,point head to the newNode
         Head=newNode;
     }
+    public void addLast(int data){
+        Node newNode=new Node(data);
+        if(Tail==null){
+            Head=Tail=newNode;
+            return;
+        }
+        Tail.next=newNode;
+        Tail=newNode;
+    }
+
+    public void print(){
+        Node temp=Head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
 
     public static void main(String[] args) {
         LinkedList ll=new LinkedList();
-        ll.addFirst(1);
-        ll.addFirst(2);
-
-        System.out.println(Head.data);
-        System.out.println(Tail.data);
+        ll.print();
+        ll.addFirst(10);
+        ll.print();
+        ll.addFirst(20);
+        ll.print();
+        // System.out.println(Head.data);
+        // System.out.println(Tail.data);
         ll.addFirst(3);
-        System.out.println(Head.data);
-        System.out.println(Tail.data);
-
+        ll.print();
+        ll.addLast(1);
+        ll.addFirst(2);
+        ll.print();
+        ll.addLast(90);
+        ll.print();
+        ll.addFirst(100);
+        ll.print();
     }
 }
