4d81abaadbb104c2c011fcdc840877a3208df3a4
K VEERESH <kveeresh288@gmail.com>
2025-05-11 16:32:46 +0530

    Linked List

--- LinkedList/Implementation/LinkedList.java
+++ LinkedList/Implementation/LinkedList.java
@@ -11,10 +11,11 @@ public class LinkedList{
     }
     static Node Head;
     static Node Tail;
-
+    static int size=0;
     void addFirst(int data){
          //step1 create a new Node 
          Node newNode=new Node(data);
+         size++;
         if(Head==null){
             Head=Tail=newNode;
             return;
@@ -26,6 +27,7 @@ public class LinkedList{
     }
     public void addLast(int data){
         Node newNode=new Node(data);
+        size++;
         if(Tail==null){
             Head=Tail=newNode;
             return;
@@ -33,6 +35,119 @@ public class LinkedList{
         Tail.next=newNode;
         Tail=newNode;
     }
+    public void addMiddle(int idx,int data){
+        if(idx<0)return;
+        Node newNode = new Node(data);
+        if(Head==null||idx==0){
+            addFirst(data);
+            return;
+        }
+        //idx%=size;
+        if(idx>=size){
+            addLast(data);
+            return;
+        }
+        //size++;
+        Node temp=Head;
+        int i=0;
+        while(temp!=null&&i<idx-1){    
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+        size++;
+}
+//delete first 
+public void deleteFirst(){
+    if(Head==null){
+        System.out.println("The list is empty: ");
+        return ;
+    }
+    System.out.println("Deleted: "+Head.data);
+    Head=Head.next;
+    if(Head==null){
+        Tail=null;
+    }
+    size--;
+}
+//delete last 
+public void deleteLast(){
+    if(Head==null){
+        System.out.println("The list is empty: ");
+        return ;
+    }
+    size--;
+    if(Head==Tail){
+        System.out.println("Deleted: " + Head.data);
+        Head=Tail=null;
+        return;
+    }
+    Node t=Head;
+    while(t!=null&&t.next!=Tail){
+        t=t.next;
+    }
+    System.out.println("Deleted: " + Tail.data);
+    Tail=t;
+    t.next=null;
+
+}
+//delete middle 
+public void deleteAt(int idx){
+    if(idx <0 || idx>=size){
+        System.out.println("Index out of bound:"+ idx + "for Len "+size);
+        return;
+    } 
+    if(idx==0){
+        deleteFirst();
+        return;
+    }
+    if(idx==size-1){
+        deleteLast();
+        return;
+    }
+    
+    int i=0;
+    Node temp=Head;
+    while(temp!=null&&i<idx-1){
+        temp=temp.next;
+        i++;
+    }
+    temp.next=temp.next.next;
+    size--;
+}
+
+//find mid idx of linked list 
+public int findMid(){
+    //Node i=Head;
+    if(Head==null){
+        System.out.println("List is Empty: ");
+        return -1;
+    }
+    int mid=0;
+    Node Fast=Head;
+    while(Fast!=null&&Fast.next!=null){
+        Fast=Fast.next.next;
+        mid++;
+    }
+    return mid;
+}
+//reverse linked list 
+public void reverse(){
+    if(Head==null||Head.next==null)return;
+    
+    Node Prev=null;
+    Node Cur=Head;
+    Tail=Head;
+    while(Cur!=null){
+        Node Next=Cur.next;
+        Cur.next=Prev;
+        Prev=Cur;
+        Cur=Next;
+    }
+   
+    Head=Prev;
+}
 
     public void print(){
         Node temp=Head;
@@ -71,5 +186,10 @@ public class LinkedList{
         ll.addFirst(100);
         ll.print();
         System.out.println("Length of Linked List is : "+ll.len());
+        ll.addMiddle(4, 68);
+        ll.print();
+        System.out.println("Length of Linked List is : "+ll.size);
+        System.out.println(size);
+
     }
 }
